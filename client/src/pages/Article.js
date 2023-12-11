c9edecdd8081f858c6fa3dbe30efe08d14edbae2
MuKuSh11 <munesh.developer@gmail.com>
2023-12-11 16:16:46 +0530

    Making article list as reusable component

--- client/src/pages/Article.js
+++ client/src/pages/Article.js
@@ -2,6 +2,9 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import ArticleContent from "./article-content";
 
+// Components
+import Articles from "../components/Articles";
+
 const Article = () => {
     const { name } = useParams();
     const article = ArticleContent.find((article) => article.name === name );
@@ -9,6 +12,7 @@ const Article = () => {
         return (
             <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900 text-center'>Article does not exist</h1>
         )
+    const otherArticles = ArticleContent.filter(article => article.name !== name);
     return (
         <div>
             <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
@@ -19,6 +23,12 @@ const Article = () => {
                     {paragraph}
                 </p>
             ))}
+            <h1 className="sm:text-2xl text-xl font-bold my-4 text-gray-900">
+                Other Articles
+            </h1>
+            <div className="flex flex-wrap -m-4">
+                <Articles articles={otherArticles} />
+            </div>
         </div>
     )
 }
